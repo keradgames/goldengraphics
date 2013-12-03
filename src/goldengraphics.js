278f86f5c430652a620e039d49e0bae5d79ac93c
Fernando Drumond <fernando.drumond@goldenmanager.com>
2013-12-03 15:14:19 +0100

    Class Inheritance implementation

--- src/goldengraphics.js
+++ src/goldengraphics.js
@@ -1,26 +1,102 @@
 (function(exports){
   var GoldenGraphics = {};
 
+  /* Simple JavaScript Inheritance
+   * By John Resig http://ejohn.org/
+   * MIT Licensed.
+   */
+  // Inspired by base2 and Prototype
+  (function(){
+    var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
 
-  GoldenGraphics.CanvasRenderer = function(canvas){
-    this.canvas = canvas;
-    this.context = this.canvas.getContext("2d");
-    this.stage = new GoldenGraphics.Stage(this);
-    this.imageData = this.context.createImageData(this.canvas.width, this.canvas.height);
+    // The base Class implementation (does nothing)
+    this.Class = function(){};
 
-    GoldenGraphics.CanvasRenderer.instance = this;
-  };
+    // Create a new Class that inherits from this class
+    Class.extend = function(prop) {
+      var _super = this.prototype;
 
-  GoldenGraphics.CanvasRenderer.prototype.render = function() {
-    // clear canvas
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      // Instantiate a base class (but only create the instance,
+      // don't run the init constructor)
+      initializing = true;
+      var prototype = new this();
+      initializing = false;
 
-    this.stage.updateImageData();
+      // Copy the properties over onto the new prototype
+      for (var name in prop) {
+        // Check if we're overwriting an existing function
+        prototype[name] = typeof prop[name] == "function" &&
+          typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+          (function(name, fn){
+            return function() {
+              var tmp = this._super;
 
-    if(this.stage.imageData){
-      this.context.putImageData(this.stage.imageData, 0, 0);
+              // Add a new ._super() method that is the same method
+              // but on the super-class
+              this._super = _super[name];
+
+              // The method only need to be bound temporarily, so we
+              // remove it when we're done executing
+              var ret = fn.apply(this, arguments);
+              this._super = tmp;
+
+              return ret;
+            };
+          })(name, prop[name]) :
+          prop[name];
+      }
+
+      // The dummy class constructor
+      function Class() {
+        // All construction is actually done in the init method
+        if ( !initializing && this.init )
+          this.init.apply(this, arguments);
+      }
+
+      // Populate our constructed prototype object
+      Class.prototype = prototype;
+
+      // Enforce the constructor to be what we expect
+      Class.prototype.constructor = Class;
+
+      // And make this class extendable
+      Class.extend = arguments.callee;
+
+      return Class;
+    };
+  })();
+
+  // CORE
+
+  GoldenGraphics.Base = Class.extend({
+    init : function(){}
+  });
+
+
+
+  // RENDERING
+
+  GoldenGraphics.CanvasRenderer = GoldenGraphics.Base.extend({
+    init: function(canvas){
+      this.canvas = canvas;
+      this.context = this.canvas.getContext("2d");
+      this.stage = new GoldenGraphics.Stage(this);
+      this.imageData = this.context.createImageData(this.canvas.width, this.canvas.height);
+
+      GoldenGraphics.CanvasRenderer.instance = this;
+    },
+
+    render: function() {
+      // clear canvas
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      this.stage._updateImageData();
+
+      if(this.stage.imageData){
+        this.context.putImageData(this.stage.imageData, 0, 0);
+      }
     }
-  };
+  });
 
   // Static functions and properties
 
@@ -56,142 +132,188 @@
     return clone;
   };
 
+  // DISPLAY OBJECT CONTAINER
 
-  // STAGE
-  // TODO create Display Object Container
+  GoldenGraphics.DisplayObjectContainer = GoldenGraphics.Base.extend({
+    init : function(){
+      this.children = [];
+      this.imageData = null;
+      this.parent = null;
+    },
 
-  GoldenGraphics.Stage = function(renderer){
-    this.renderer = renderer;
-    this.children = [];
-    this.imageData = null;
-  }
+    addChild : function(child){
+      if(this.children.indexOf(child) < 0){
+        this.children.push(child);
+      }
 
-  GoldenGraphics.Stage.prototype.addChild = function(child) {
-    this.children.push(child);
-  };
+      child.parent = this;
+      child.stage = this.stage;
+    },
 
-  GoldenGraphics.Stage.prototype.updateImageData = function(){
-    var l = this.children.length;
+    removeChild : function(){
+      // TODO
+    },
 
-    var pos = 0;
-    var inpos = 0;
-    var renderData = null;
-    var child = null;
-    var imageData = null;
+    // private properties
 
-    this.imageData = this.renderer.context.createImageData(this.renderer.canvas.width, this.renderer.canvas.height);
+    _updateImageData: function(){
+      var l = this.children.length;
 
-    var dataLength = 0;
-    var matrixSize = 0;
-    var r = 0;
-    var g = 0;
-    var b = 0;
-    var a = 0;
-    var a0 = 0;
-    var af = 0;
-    var x = 0;
-    var y = 0;
-    var pos_x = 0;
-    var pos_y = 0;
+      var pos = 0;
+      var inpos = 0;
+      var renderData = null;
+      var child = null;
+      var imageData = null;
 
-    var _log = "";
+      // TODO do not create new image data
 
-    // for each child
-    for(var i =0; i < l; i++){
-      child = this.children[i];
-      imageData = child.imageData;
-      x = Math.round(child.position.x);
-      y = Math.round(child.position.y);
+      this.imageData = this.renderer.context.createImageData(this.renderer.canvas.width, this.renderer.canvas.height);
 
-      if(child.opacity > 0 && imageData){
-        matrixSize = this.imageData.data.length - 1;
+      var dataLength = 0;
+      var matrixSize = 0;
+      var r = 0;
+      var g = 0;
+      var b = 0;
+      var a = 0;
+      var a0 = 0;
+      var af = 0;
+      var x = 0;
+      var y = 0;
+      var pos_x = 0;
+      var pos_y = 0;
 
-        child.applyFilters();
+      var _log = "";
 
-        for(var j = 0; j < imageData.width; j++){
-          for(var k = 0; k < imageData.height; k++){
-            pos = (j * imageData.width + k) * 4;
-            pos_x = k + x;
-            pos_y = j + y;
+      // for each child
+      for(var i =0; i < l; i++){
+        child = this.children[i];
+        imageData = child.imageData;
+        x = Math.round(child.position.x);
+        y = Math.round(child.position.y);
 
-            // only render pixels inside the screen, avoid mirror effect
-            if(pos_x >= 0 && pos_y >= 0 && pos_x < this.imageData.width && pos_y < this.imageData.height){
-              renderPos = (pos_y * this.imageData.width + pos_x) * 4;
+        if(child.opacity > 0 && imageData){
+          matrixSize = this.imageData.data.length - 1;
 
-              // _log += renderPos + " ,";
+          child.applyFilters();
 
-              r = imageData.data[pos+0];
-              g = imageData.data[pos+1];
-              b = imageData.data[pos+2];
-              a0 = imageData.data[pos+3];
+          for(var j = 0; j < imageData.width; j++){
+            for(var k = 0; k < imageData.height; k++){
+              pos = (j * imageData.width + k) * 4;
+              pos_x = k + x;
+              pos_y = j + y;
 
-              a = a0 / 255 * child.opacity; //normaliza alpha between 0 and 1 and apply opacity
+              // only render pixels inside the screen, avoid mirror effect
+              if(pos_x >= 0 && pos_y >= 0 && pos_x < this.imageData.width && pos_y < this.imageData.height){
+                renderPos = (pos_y * this.imageData.width + pos_x) * 4;
 
-              // check render for pixels with alpha > 0
-              if(a > 0 && renderPos < matrixSize){
-                af = this.imageData.data[renderPos+3] / 255 || 1;
+                // _log += renderPos + " ,";
 
-                this.imageData.data [renderPos+0] = r * a + (1 - a) * this.imageData.data [renderPos+0] * af;
-                // _log += renderPos + " " + this.imageData.data [renderPos] + ", ";
-                this.imageData.data [renderPos+1] = g * a + (1 - a) * this.imageData.data [renderPos+1] * af;
-                this.imageData.data [renderPos+2] = b * a + (1 - a) * this.imageData.data [renderPos+2] * af;
-                this.imageData.data [renderPos+3] = Math.max(a0 * a, this.imageData.data [renderPos+3]);
+                r = imageData.data[pos+0];
+                g = imageData.data[pos+1];
+                b = imageData.data[pos+2];
+                a0 = imageData.data[pos+3];
+
+                a = a0 / 255 * child.opacity; //normaliza alpha between 0 and 1 and apply opacity
+
+                // check render for pixels with alpha > 0
+                if(a > 0 && renderPos < matrixSize){
+                  af = this.imageData.data[renderPos+3] / 255 || 1;
+
+                  this.imageData.data [renderPos+0] = r * a + (1 - a) * this.imageData.data [renderPos+0] * af;
+                  // _log += renderPos + " " + this.imageData.data [renderPos] + ", ";
+                  this.imageData.data [renderPos+1] = g * a + (1 - a) * this.imageData.data [renderPos+1] * af;
+                  this.imageData.data [renderPos+2] = b * a + (1 - a) * this.imageData.data [renderPos+2] * af;
+                  this.imageData.data [renderPos+3] = Math.max(a0 * a, this.imageData.data [renderPos+3]);
+                }
               }
             }
           }
         }
       }
-    }
 
-    if(_log && _log.length > 0){
-      console.log(_log);
-    }
+      if(_log && _log.length > 0){
+        console.log(_log);
+      }
 
-  };
+    }
+  })
+
+
+  // STAGE
+  // TODO create Display Object Container
+
+  GoldenGraphics.Stage = GoldenGraphics.DisplayObjectContainer.extend({
+    init : function(renderer){
+      this._super();
+      this.renderer = renderer;
+    }
+  });
+
+
 
 
   // COLOR
 
-  GoldenGraphics.Color = function(r, g, b, a){
+  GoldenGraphics.Color = GoldenGraphics.Base.extend({
+    init: function(r, g, b, a){
+      this._super();
 
-    this.r = r || 0;
-    this.g = g || 0;
-    this.b = b || 0;
-    this.a = a != undefined ? a : 1;
+      this.r = r || 0;
+      this.g = g || 0;
+      this.b = b || 0;
+      this.a = a != undefined ? a : 1;
+    },
 
-  }
-
-  GoldenGraphics.Color.prototype.multiply = function(color) {
-    return color;
-  };
-
-  // POSITION
-
-  GoldenGraphics.Position = function(x, y){
-    this.x = x || 0;
-    this.y = y || 0;
-  }
-
-
-
-
-  GoldenGraphics.Sprite = function(image){
-    var _this = this;
-    this.cachedImageData = null;
-    this.imageData = null;
-    this.stage = null;
-    this.position = new GoldenGraphics.Position();
-    this.filters = [];
-    this.opacity = 1;
-
-    function onImageLoad (){
-      _this.cachedImageData = GoldenGraphics.CanvasRenderer.getImageData(this);
-      _this.imageData = GoldenGraphics.CanvasRenderer.cloneImageData(_this.cachedImageData);
+    multiply: function(color) {
+      return color;
     }
+  });
 
-    image.addEventListener("load", onImageLoad);
-  };
 
+  // POINT
+
+  GoldenGraphics.Point2D = GoldenGraphics.Base.extend({
+    init: function(x, y){
+      this._super();
+
+      this.x = x || 0;
+      this.y = y || 0;
+    }
+  })
+
+
+  // SPRITE
+
+  GoldenGraphics.Sprite = GoldenGraphics.DisplayObjectContainer.extend({
+    init: function(image){
+      this._super();
+
+      var _this = this;
+      this.cachedImageData = null;
+      this.position = new GoldenGraphics.Point2D();
+      this.filters = [];
+      this.opacity = 1;
+
+      function onImageLoad (){
+        _this.cachedImageData = GoldenGraphics.CanvasRenderer.getImageData(this);
+        _this.imageData = GoldenGraphics.CanvasRenderer.cloneImageData(_this.cachedImageData);
+      }
+
+      image.addEventListener("load", onImageLoad);
+    },
+
+    applyFilters: function(){
+      for(var i = 0; i < this.filters.length; i++){
+        this.filters[i].apply(this.cachedImageData, this.imageData);
+      }
+    },
+
+    addChild: function(child){
+      this.children.push(child);
+    }
+  });
+
+  // Static functions and properties
 
   GoldenGraphics.Sprite.fromImageUrl = function(url){
     var image = new Image();
@@ -203,12 +325,6 @@
     return sprite;
   }
 
-  GoldenGraphics.Sprite.prototype.applyFilters = function(){
-    for(var i = 0; i < this.filters.length; i++){
-      this.filters[i].apply(this.cachedImageData, this.imageData);
-    }
-  }
-
 
   // FILTERS
 
@@ -217,56 +333,55 @@
   // TintFilter (color)
   // TintFilter (r, g, b, a)
 
-  GoldenGraphics.filters.TintFilter = function(){
-    var color = null;
+  GoldenGraphics.filters.TintFilter = GoldenGraphics.Base.extend({
+    init: function(){
+      this._super();
+      var color = null;
 
-    if(arguments.length == 1){
-      color = arguments[0];
-    }
-    else{
-      color = new GoldenGraphics.Color(arguments[0], arguments[1], arguments[2], arguments[3]);
-    }
+      if(arguments.length == 1){
+        color = arguments[0];
+      }
+      else{
+        color = new GoldenGraphics.Color(arguments[0], arguments[1], arguments[2], arguments[3]);
+      }
 
-    this.color = color;
-  }
+      this.color = color;
+    },
 
-  GoldenGraphics.filters.TintFilter.prototype.apply = function(origin, target) {
-    var pos = 0;
-    var inpos = 0;
+    apply: function(origin, target) {
+      var pos = 0;
+      var inpos = 0;
 
-    var r = this.color.r;
-    var g = this.color.g;
-    var b = this.color.b;
+      var r = this.color.r;
+      var g = this.color.g;
+      var b = this.color.b;
 
-    var _log = "";
+      var _log = "";
 
-    for (var i = 0; i < origin.width; i ++){
-      for (var j = 0; j < origin.height; j ++){
-        var r_0 = origin.data [inpos++];
-        var g_0 = origin.data [inpos++];
-        var b_0 = origin.data [inpos++];
-        var a_0 = origin.data [inpos++];
+      for (var i = 0; i < origin.width; i ++){
+        for (var j = 0; j < origin.height; j ++){
+          var r_0 = origin.data [inpos++];
+          var g_0 = origin.data [inpos++];
+          var b_0 = origin.data [inpos++];
+          var a_0 = origin.data [inpos++];
 
-        if (a_0 > 0){
-          target.data [pos] = r * origin.data [pos] / 255;
-          pos ++;
-          target.data [pos] = g * origin.data [pos] / 255;
-          pos ++;
-          target.data [pos] = b * origin.data [pos] / 255;
-          pos ++;
-          target.data [pos] = origin.data [pos];
-          pos ++;
-        }
-        else{
-          pos += 4;
+          if (a_0 > 0){
+            target.data [pos] = r * origin.data [pos] / 255;
+            pos ++;
+            target.data [pos] = g * origin.data [pos] / 255;
+            pos ++;
+            target.data [pos] = b * origin.data [pos] / 255;
+            pos ++;
+            target.data [pos] = origin.data [pos];
+            pos ++;
+          }
+          else{
+            pos += 4;
+          }
         }
       }
     }
-  }
-
-
-
-
+  });
 
 
   exports.GoldenGraphics = GoldenGraphics;
