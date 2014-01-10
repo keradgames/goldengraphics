29988bdd7fa2aadbb1a89fefbc3c0ae7798ddb5a
Fernando Drumond <fernando.drumond@goldenmanager.com>
2014-01-10 10:00:31 +0100

    Rendering optimization (fixed)

--- bin/goldengraphics.js
+++ bin/goldengraphics.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2014 Kerad Games S. L. 
- // goldengraphics 2014-01-09 
+ // goldengraphics 2014-01-10 
   /* The MIT License (MIT) 
  
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
@@ -427,14 +427,9 @@ GoldenGraphics.AssetLoader = GoldenGraphics.Base.extend({
       if(stage){
         this._updateImageData(stage);
 
-
-        // if(stage.imageData){
-        //   this.context.putImageData(stage.imageData, 0, 0);
-        // }
-
-        // if(stage._render && stage._render._imageToRender){
-        //   this.context.drawImage(stage._render._imageToRender, 0, 0, stage._render._imageToRender.width, stage._render._imageToRender.height);
-        // }
+        if(stage._render && stage._render._imageToRender){
+          this.context.drawImage(stage._render._imageToRender, 0, 0, stage._render._imageToRender.width, stage._render._imageToRender.height);
+        }
       }
       else{
         console.log('Stage is not defined');
@@ -442,7 +437,7 @@ GoldenGraphics.AssetLoader = GoldenGraphics.Base.extend({
 
       var t1 = (new Date()).getTime();
 
-      console.log("render", (t1 - t0));
+      // console.log("render", (t1 - t0));
 
 
     },
@@ -453,30 +448,12 @@ GoldenGraphics.AssetLoader = GoldenGraphics.Base.extend({
     },
 
     _updateImageData: function(displayObject){
-      var pos = 0;
-      var renderData = null;
-      var childImageData = null;
       var child = null;
 
-      // TODO do not create new image data
-      var renderData = null;
 
-      var dataLength = 0;
-      var matrixSize = 0;
-      var r = 0;
-      var g = 0;
-      var b = 0;
-      var a = 0;
-      var a0 = 0;
-      var af = 0;
       var x = 0;
       var y = 0;
-      var pos_x = 0;
-      var pos_y = 0;
-      var renderDataWidth = 0;
-      var renderDataHeight = 0;
-      var childImageWidth = 0;
-      var childImageHeight = 0;
+
 
       var _log = "";
 
@@ -516,24 +493,7 @@ GoldenGraphics.AssetLoader = GoldenGraphics.Base.extend({
               this._updateImageData(child);
 
               if(child._render._imageToRender){
-
-                var t0 = (new Date()).getTime();
-                var t1 = (new Date()).getTime();
-                var limit = 500;
-
-                while(!child._render._imageToRender.complete && t1 - t0 < limit){
-                  t1 = (new Date()).getTime();
-                }
-
-                if(child._render._imageToRender.complete){
-                  displayObject._render._context.drawImage(child._render._imageToRender, x, y, child._render._imageToRender.naturalWidth, child._render._imageToRender.naturalHeight);
-                }
-                else{
-                  console.log(child._render._imageToRender.complete, child._render._imageToRender.naturalWidth, child._render._imageToRender.naturalHeight);
-                  child._render._imageToRender.onload = function(){
-
-                  };
-                }
+                displayObject._render._context.drawImage(child._render._imageToRender, x, y, child._render._imageToRender.width, child._render._imageToRender.height);
               }
             }
           }
@@ -541,19 +501,16 @@ GoldenGraphics.AssetLoader = GoldenGraphics.Base.extend({
 
         if(displayObject.filters && displayObject.filters.length > 0){
           this._applyFilters(displayObject);
-          displayObject._render._imageToRender.src = displayObject._render._canvas.toDataURL();
+          displayObject._render._imageToRender = displayObject._render._canvas;
         }
         else{
           if(displayObject.children.length > 0){
-            displayObject._render._imageToRender.src = displayObject._render._canvas.toDataURL();
+            displayObject._render._imageToRender = displayObject._render._canvas;
           }
           else{
             displayObject._render._imageToRender = displayObject.texture;
           }
         }
-
-
-        // console.log("img", displayObject._render._imageToRender.src);
 
       }
     },
@@ -568,23 +525,17 @@ GoldenGraphics.AssetLoader = GoldenGraphics.Base.extend({
     _createTextureCanvas: function(displayObject){
       displayObject._render = displayObject._render || {};
 
-      if(displayObject.stage == displayObject){
-        displayObject._render._canvas = this.canvas;
-        displayObject._render._context = this.context;
+      displayObject._render._canvas = document.createElement('canvas');
+      displayObject._render._context = displayObject._render._canvas.getContext('2d');
+      // document.body.appendChild(displayObject._render._canvas);
+
+      if(displayObject.texture){
+        displayObject._render._canvas.width = displayObject.texture.width;
+        displayObject._render._canvas.height = displayObject.texture.height;
       }
       else{
-        displayObject._render._canvas = document.createElement('canvas');
-        displayObject._render._context = displayObject._render._canvas.getContext('2d');
-        document.body.appendChild(displayObject._render._canvas);
-
-        if(displayObject.texture){
-          displayObject._render._canvas.width = displayObject.texture.width;
-          displayObject._render._canvas.height = displayObject.texture.height;
-        }
-        else{
-          displayObject._render._canvas.width = this.canvas.width;
-          displayObject._render._canvas.height = this.canvas.height;
-        }
+        displayObject._render._canvas.width = this.canvas.width;
+        displayObject._render._canvas.height = this.canvas.height;
       }
     },
 
--- src/goldengraphics/render/CanvasRendering.js
+++ src/goldengraphics/render/CanvasRendering.js
@@ -17,14 +17,9 @@
       if(stage){
         this._updateImageData(stage);
 
-
-        // if(stage.imageData){
-        //   this.context.putImageData(stage.imageData, 0, 0);
-        // }
-
-        // if(stage._render && stage._render._imageToRender){
-        //   this.context.drawImage(stage._render._imageToRender, 0, 0, stage._render._imageToRender.width, stage._render._imageToRender.height);
-        // }
+        if(stage._render && stage._render._imageToRender){
+          this.context.drawImage(stage._render._imageToRender, 0, 0, stage._render._imageToRender.width, stage._render._imageToRender.height);
+        }
       }
       else{
         console.log('Stage is not defined');
@@ -32,7 +27,7 @@
 
       var t1 = (new Date()).getTime();
 
-      console.log("render", (t1 - t0));
+      // console.log("render", (t1 - t0));
 
 
     },
@@ -43,30 +38,12 @@
     },
 
     _updateImageData: function(displayObject){
-      var pos = 0;
-      var renderData = null;
-      var childImageData = null;
       var child = null;
 
-      // TODO do not create new image data
-      var renderData = null;
 
-      var dataLength = 0;
-      var matrixSize = 0;
-      var r = 0;
-      var g = 0;
-      var b = 0;
-      var a = 0;
-      var a0 = 0;
-      var af = 0;
       var x = 0;
       var y = 0;
-      var pos_x = 0;
-      var pos_y = 0;
-      var renderDataWidth = 0;
-      var renderDataHeight = 0;
-      var childImageWidth = 0;
-      var childImageHeight = 0;
+
 
       var _log = "";
 
@@ -106,24 +83,7 @@
               this._updateImageData(child);
 
               if(child._render._imageToRender){
-
-                var t0 = (new Date()).getTime();
-                var t1 = (new Date()).getTime();
-                var limit = 500;
-
-                while(!child._render._imageToRender.complete && t1 - t0 < limit){
-                  t1 = (new Date()).getTime();
-                }
-
-                if(child._render._imageToRender.complete){
-                  displayObject._render._context.drawImage(child._render._imageToRender, x, y, child._render._imageToRender.naturalWidth, child._render._imageToRender.naturalHeight);
-                }
-                else{
-                  console.log(child._render._imageToRender.complete, child._render._imageToRender.naturalWidth, child._render._imageToRender.naturalHeight);
-                  child._render._imageToRender.onload = function(){
-
-                  };
-                }
+                displayObject._render._context.drawImage(child._render._imageToRender, x, y, child._render._imageToRender.width, child._render._imageToRender.height);
               }
             }
           }
@@ -131,19 +91,16 @@
 
         if(displayObject.filters && displayObject.filters.length > 0){
           this._applyFilters(displayObject);
-          displayObject._render._imageToRender.src = displayObject._render._canvas.toDataURL();
+          displayObject._render._imageToRender = displayObject._render._canvas;
         }
         else{
           if(displayObject.children.length > 0){
-            displayObject._render._imageToRender.src = displayObject._render._canvas.toDataURL();
+            displayObject._render._imageToRender = displayObject._render._canvas;
           }
           else{
             displayObject._render._imageToRender = displayObject.texture;
           }
         }
-
-
-        // console.log("img", displayObject._render._imageToRender.src);
 
       }
     },
@@ -158,23 +115,17 @@
     _createTextureCanvas: function(displayObject){
       displayObject._render = displayObject._render || {};
 
-      if(displayObject.stage == displayObject){
-        displayObject._render._canvas = this.canvas;
-        displayObject._render._context = this.context;
+      displayObject._render._canvas = document.createElement('canvas');
+      displayObject._render._context = displayObject._render._canvas.getContext('2d');
+      // document.body.appendChild(displayObject._render._canvas);
+
+      if(displayObject.texture){
+        displayObject._render._canvas.width = displayObject.texture.width;
+        displayObject._render._canvas.height = displayObject.texture.height;
       }
       else{
-        displayObject._render._canvas = document.createElement('canvas');
-        displayObject._render._context = displayObject._render._canvas.getContext('2d');
-        document.body.appendChild(displayObject._render._canvas);
-
-        if(displayObject.texture){
-          displayObject._render._canvas.width = displayObject.texture.width;
-          displayObject._render._canvas.height = displayObject.texture.height;
-        }
-        else{
-          displayObject._render._canvas.width = this.canvas.width;
-          displayObject._render._canvas.height = this.canvas.height;
-        }
+        displayObject._render._canvas.width = this.canvas.width;
+        displayObject._render._canvas.height = this.canvas.height;
       }
     },
 
